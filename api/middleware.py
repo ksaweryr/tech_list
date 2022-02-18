3cea9516faf89ada89b058a5fe3bf446f199a24f
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2022-02-18 16:03:58 +0100

    Add login_required decorator to api

--- api/middleware.py
+++ api/middleware.py
@@ -1,7 +1,20 @@
 from .utils import error
-from flask import request
+from flask import g, request
+from functools import wraps
+from typing import Callable
 
 
 def reject_non_json():
     if not request.is_json:
         return error('only json format is supported'), 400
+
+
+def login_required(f: Callable) -> Callable:
+    @wraps(f)
+    def inner(*args, **kwargs):
+        if g.user is None:
+            return error('you must be authorized to perform this action'), 401
+
+        return f(*args, **kwargs)
+
+    return inner
--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from .api import bp as api
+from .utils import User
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, g, request
+from jwt.exceptions import InvalidTokenError
+from os import environ
+
+import jwt
 
 load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api')
+
+
+@app.before_request
+def set_g_user():
+    g.user = None
+    token = request.cookies.get('token')
+
+    if token is None:
+        return
+
+    try:
+        token = jwt.decode(token, environ['TOKEN'], algorithms=['HS256'],
+            options={
+                'require': ['username', 'admin', 'iat']
+            }
+        )
+    except InvalidTokenError:
+        return
+
+    g.user = User(token['username'], token['admin'])
 
 
 @app.get('/ping')
